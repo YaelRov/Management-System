71ff058aa8faab6648667fdef3e7785090d5c7e4
Yoni4517 <yonistern15@gmail.com>
2023-10-25 00:41:51 +0300

    Initial Code + Welcome functions

---
+++ Stage0/Program9215.cs
@@ -0,0 +1,18 @@
+﻿partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Welcome9215();
+        Welcome2620();
+        Console.ReadKey();
+    }
+    static partial void Welcome2620();
+    private static void Welcome9215()
+    {
+        Console.WriteLine("Enter your name: ");
+        string name = Console.ReadLine();
+        Console.WriteLine($"{name}, welcome to my first console application");
+    }
+
+
+}
