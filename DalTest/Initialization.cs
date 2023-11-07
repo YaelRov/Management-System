64eca0d6d685b08937ec2bae0c87d63bfeeff3b5
Yoni4517 <yonistern15@gmail.com>
2023-11-07 21:05:12 +0200

    nothing

--- DalTest/Initialization.cs
+++ DalTest/Initialization.cs
@@ -14,25 +14,50 @@ public static class Initialization
     #region methods
     private static void createTasks()
     {
-
-    }
-    private static void createDependencies()
-    {
-
-    }
-    private static void createEngineers()
-    {
-        string[] firstNames =
+        string[] buildingTasks = new string[]
         {
+            "Measure and mark the dimensions of the foundation.",
+            "Dig the foundation trench to the required depth.",
+            "Pour and level the concrete foundation.",
+            "Construct the framework for the walls.",
+            "Install electrical and plumbing systems.",
+            "Insulate the walls and ceiling for energy efficiency.",
+            "Install doors, windows, and roofing materials.",
+            "Finish the walls with drywall or other materials.",
+            "Paint the interior and exterior surfaces.",
+            "Install flooring and finish with baseboards.",
+            "Connect utilities and perform final inspections.",
+            "Set up scaffolding for elevated work areas.",
+            "Lay bricks or blocks for walls.",
+            "Install insulation in the attic.",
+            "Mount cabinets and countertops in the kitchen.",
+            "Install lighting fixtures and switches.",
+            "Apply exterior siding for weather protection.",
+            "Hang interior doors and install hardware.",
+            "Install plumbing fixtures in bathrooms.",
+            "Apply final coat of paint to all surfaces."
+        };
+    };
+}
+private static void createDependencies()
+{
+
+}
+private static void createEngineers()
+{
+    string[] firstNames =
+    {
             "Avraham", "Yitzchak", "Yaakov", "Moshe", "Aharon", "Yossef",
             "David", "Chaim", "Refael", "Menachem", "Uriel", "Shlomo"
         };
-        string[] lastNames =
-        {
+    string[] lastNames =
+    {
             "Stern", "Rov", "Gold", "Silver", "Weiss", "Cohen", "Levi",
             "Zusman", "Elkias", "Elchadad", "Shachar", "Maimon", "Toledano"
         };
 
-    }
+
+
+}
     #endregion
 }
