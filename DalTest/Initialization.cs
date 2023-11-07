3dc83ba7a7cb4ad5da5fc68d2819323c9b8db0cf
Yoni4517 <yonistern15@gmail.com>
2023-11-07 21:11:27 +0200

    tasks array

--- DalTest/Initialization.cs
+++ DalTest/Initialization.cs
@@ -42,29 +42,30 @@ public static class Initialization
 private static void createDependencies()
 {
 
-    }
-    private static void createEngineers()
+}
+private static void createEngineers()
+{
+    const int MIN_ID = 200000000;
+    const int MAX_ID = 400000000;
+    string[] names =
     {
-        const int MIN_ID = 200000000;
-        const int MAX_ID = 400000000;
-        string[] names =
-        {
             "Yoni Stern", "Avraham Silver", "Itzchak Elkias", "Yaakov Beker",
             "Refael Toledano", "Aharon Zusman"
         };
-        foreach name in names
+    foreach name in names
         {
-            int id;
-            do
-                id = s_rand.Next(MIN_ID, MAX_ID);
-            while (Engineers!.Read(_id) != null);
-            double cost = s_rand.Next(30, 100);
-            EngineerExperience level = s_rand.Next(0, 3);
-            string email = name.Replace(" ", "");
-            email += "@gmail.com";
-            Engineer eng = new(id, nameof, email, level, cost);
-            Engineers.Create(eng);
-        }
+        int id;
+        do
+            id = s_rand.Next(MIN_ID, MAX_ID);
+        while (Engineers!.Read(_id) != null);
+        double cost = s_rand.Next(30, 100);
+        EngineerExperience level = s_rand.Next(0, 3);
+        string email = name.Replace(" ", "");
+        email += "@gmail.com";
+        Engineer eng = new(id, nameof, email, level, cost);
+        Engineers.Create(eng);
+        s_dalEngineer++;
     }
+}
     #endregion
 }
