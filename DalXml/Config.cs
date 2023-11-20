4d53fa5415914933489a8d98f1ed05978acfd3c4
Yoni4517 <yonistern15@gmail.com>
2023-11-20 19:41:14 +0200

    unit 1-3 stage 3

---
+++ DalXml/Config.cs
@@ -0,0 +1,17 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+namespace Dal;
+
+internal static class Config
+{
+    static string s_data_config_xml = "data-config";
+
+    //זה היה בדוגמא, אבל לא פירטו על זה כלום....
+    //internal static int NextTaskId { get => XMLTools.GetAndIncreaseNextId(s_data_config_xml, "NextTaskId"); }
+    //internal static int NextDependencyId { get => XMLTools.GetAndIncreaseNextId(s_data_config_xml, "NextDependencyId"); }
+
+}
