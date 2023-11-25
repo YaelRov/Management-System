fefaa6e0512f7bb0015e3db493522ada2bb3f9e4
YaelRov <yael223590@gmail.com>
2023-11-26 01:42:42 +0200

    tr

--- DalXml/EngineerImplementation.cs
+++ DalXml/EngineerImplementation.cs
@@ -13,22 +13,23 @@ internal class EngineerImplementation : IEngineer
     {
         //checking if this engineer is exists already
         Engineer? isExistEngineer = Read(item.Id);
-        if (isExistEngineer is not null)
+        if(isExistEngineer is not null)
             throw new DalAlreadyExistsException($"An object of type Engineer with ID {item.Id} already exists");
 
         XElement? xmlEngineersFileRoot = XMLTools.LoadListFromXMLElement("engineers");
+        //checking if the root element "Engineers" exists
+        XElement? xmlEngineers = xmlEngineersFileRoot.Descendants("Engineers").FirstOrDefault();
+        //creating the root element "Engineers" if it wasn't exist
+        xmlEngineers ??= new XElement("Engineers");
 
-        //creating the new "Engineer" element
-        XElement newEng = new XElement("Engineer",
-                                       item.Name,
-                                       new XAttribute("Id", item.Id),
-                                       new XAttribute("Email", item.Email),
-                                       new XAttribute("Level", item.Level),
-                                       new XAttribute("Cost", item.Cost));
-
-        xmlEngineersFileRoot.Add(newEng);
-        XMLTools.SaveListToXMLElement(xmlEngineersFileRoot, "engineers");
-        Engineer.counterEngineers++;//add 1 to the counter of the engineers
+        //adding the new "Engineer" element
+        xmlEngineers!.Add(new XElement("Engineer",
+                                        new XAttribute("Id", item.Id),
+                                        new XAttribute("Email", item.Email),
+                                        new XAttribute("Level", item.Level),
+                                        new XAttribute("Cost", item.Cost)),
+                                        item.Name);
+        XMLTools.SaveListToXMLElement(xmlEngineers, "engineers");
         return item.Id;
     }
 
@@ -46,14 +47,14 @@ internal class EngineerImplementation : IEngineer
     public Engineer? Read(int id)
     {
         XElement? xmlEngineers = XMLTools.LoadListFromXMLElement("engineers");
-        XElement? eng = xmlEngineers.Descendants("Engineers")
+        XElement? eng = xmlEngineers.Descendants("engineers")
             .FirstOrDefault(engineer => int.Parse(engineer.Attribute("Id")!.Value).Equals(id));
         if (eng is null)
             return null;
         Engineer returnedEngineer = new(int.Parse(eng.Attribute("Id")!.Value),
                                         eng.Value, 
                                         eng.Attribute("Email")!.Value,
-                                        (EngineerExperience)Enum.Parse(typeof(EngineerExperience),eng.Attribute("Level")!.Value),
+                                        (EngineerExperience)Enum.Parse(typeof(EngineerExperience), eng.Attribute("Level")!.Value),
                                         double.Parse(eng.Attribute("Cost")!.Value));
         return returnedEngineer;
     }
@@ -65,22 +66,7 @@ internal class EngineerImplementation : IEngineer
 
     public IEnumerable<Engineer?> ReadAll(Func<Engineer, bool>? filter = null)
     {
-        XElement? xmlEngineers = XMLTools.LoadListFromXMLElement("engineers");
-        if (filter is null)
-            filter = (e) => true;
-        List<Engineer> engineersList = xmlEngineers.Descendants("engineers")
-            .Select(engin => {
-            Engineer engineer_t = new(
-                                        int.Parse(engin.Attribute("Id")!.Value),
-                                        engin.Value,
-                                        engin.Attribute("Email")!.Value,
-                                        (EngineerExperience)Enum.Parse(typeof(EngineerExperience), engin.Attribute("Level")!.Value),
-                                        double.Parse(engin.Attribute("Cost")!.Value)
-                                                );
-                         return engineer_t; })
-            .Where(engin =>  filter(engin) )
-            .ToList();
-        return engineersList;
+        throw new NotImplementedException();
     }
 
     public void Update(Engineer item)
@@ -88,14 +74,3 @@ internal class EngineerImplementation : IEngineer
         throw new NotImplementedException();
     }
 }
-
-/*XDocument doc = XDocument.Load(@"..\xml\engineers.xml");
-doc.Root!.Add(new XElement("Engineer",
-                             new XElement("Id", item.Id),
-                             new XElement("FirstName", item.FirstName),
-                             new XElement("LastName", item.LastName),
-                             new XElement("Email", item.Email),
-                             new XElement("SalaryPerHour", item.SalaryPerHour),
-                             new XElement("EngineerLevel", item.EngineerLevel)));
-     doc.Save(@"..\xml\engineers.xml");
-     return item.Id;*/
