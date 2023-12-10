822d5740c2ecd351a8b34b9db424444cee1d4f4f
YaelRov <yael223590@gmail.com>
2023-12-10 16:23:52 +0200

    task

--- BlTest/BO/Engineer.cs
+++ BlTest/BO/Engineer.cs
@@ -1,8 +1,16 @@
 ﻿
 
+using DO;
+
 namespace BlTest.BO;
 
 public class Engineer
 {
+    public int Id { get; init; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public EngineerExperience? Level { get; set; }
+    public double Cost { get; set; }
 
+    //public override string ToString() => this.ToStringProperty();
 }
